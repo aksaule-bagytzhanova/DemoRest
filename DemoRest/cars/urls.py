5a17892a31efb8b5bab10e579c7e699660bb693b
Aksaule <aksaule124026@gmail.com>
2021-06-09 16:17:10 +0600

    Create new serializers

--- DemoRest/cars/urls.py
+++ DemoRest/cars/urls.py
@@ -1,10 +1,14 @@
+from DemoRest.cars.views import CarDetailView
 from django.urls import path
 from django.urls.conf import include
-from cars.views import *
+from cars.views import CarsListView, CarCreateView
 
 app_name = 'car'
 urlpatterns = [
     path('car/create/', CarCreateView.as_view()),
+    path('all/', CarsListView.as_view()),
+    path('car/detail/<int:pk>/', CarDetailView.as_view()),
+    
     
 
 ]
--- DemoRest/cars/views.py
+++ DemoRest/cars/views.py
@@ -1,7 +1,18 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from rest_framework import generics
-from cars.serializers import CarDetailSerializer
+from cars.serializers import CarDetailSerializer, CarsListSerializer
+from cars.models import Car
 
 # Create your views here.
 class CarCreateView(generics.CreateAPIView):
     serializer_class = CarDetailSerializer
+
+
+class CarsListView(generics.ListAPIView):
+    serializer_class = CarsListSerializer
+    queryset = Car.objects.all()
+
+class CarDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CarDetailSerializer
+    queryset= Car.objects.all()
